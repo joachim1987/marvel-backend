8e6ca3ac0b525ae3a6e1f440143f878925ad7e41
joachim1987 <bothua.joachim@live.fr>
2021-11-18 13:36:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+require('dotenv').config()
+
+const apiUrl = 'https://lereacteur-marvel-api.herokuapp.com'
+
+const app = express()
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to my API' })
+})
+
+app.get('/characters', async (req, res) => {
+  try {
+    // requête vers l'API Marvel
+    // => axios
+    const response = await axios.get(
+      `${apiUrl}/characters?apiKey=${process.env.API_KEY}`,
+    )
+    // console.log(response.data);
+    res.status(200).json(response.data)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
+
+app.get('/comics', async (req, res) => {
+  try {
+    const response = await axios.get(
+      `${apiUrl}/comics?apiKey=${process.env.API_KEY}`,
+    )
+    res.status(200).json(response.data)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
+
+app.all('*', (req, res) => {
+  res.status(404).json({ message: 'Page not found !' })
+})
+
+app.listen(4000, () => {
+  console.log('Server started !')
+})
